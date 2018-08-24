bbef21d7840d243f7499086cd83861fabbe6813d
MaxCoderCh <MaxCoderCh@users.noreply.github.com>
2018-08-24 17:44:32 +0800

    配置订单服务 调用 接口

--- AssessmenServer/src/main/java/com/sicmed/assessmen/feignService/WalletServer.java
+++ AssessmenServer/src/main/java/com/sicmed/assessmen/feignService/WalletServer.java
@@ -4,12 +4,13 @@ package com.sicmed.assessmen.feignService;
 import com.sicmed.assessmen.feignService.impl.WalletServerHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @FeignClient(value = "wallet-server", fallback = WalletServerHystrix.class)
 public interface WalletServer {
 
-    @PostMapping(value = "doctorWallet/addOrderIncome")
-    Map addOrderIncome(String orderId);
+    @PostMapping(value = "provider/addOrderIncome")
+    Map addOrderIncome(@RequestParam("orderId") String orderId);
 }
