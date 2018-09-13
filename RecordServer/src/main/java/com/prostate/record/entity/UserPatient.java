2ffe07ed37cc51b8ce1c149ff80755e07694b171
MaxCoderCh <MaxCoderCh@users.noreply.github.com>
2018-09-13 18:19:56 +0800

    优化 代码

--- RecordServer/src/main/java/com/prostate/record/entity/UserPatient.java
+++ RecordServer/src/main/java/com/prostate/record/entity/UserPatient.java
@@ -1,7 +1,13 @@
 package com.prostate.record.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+
+@Setter
+@Getter
 public class UserPatient extends BaseEntity{
     private String id;
 
@@ -11,45 +17,11 @@ public class UserPatient extends BaseEntity{
 
     private String patientSource;
 
+    private String patientType;
+
+    private String patientStatus;
+
     private Date createTime;
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id == null ? null : id.trim();
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId == null ? null : userId.trim();
-    }
-
-    public String getPatientId() {
-        return patientId;
-    }
-
-    public void setPatientId(String patientId) {
-        this.patientId = patientId == null ? null : patientId.trim();
-    }
-
-    public String getPatientSource() {
-        return patientSource;
-    }
-
-    public void setPatientSource(String patientSource) {
-        this.patientSource = patientSource == null ? null : patientSource.trim();
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
 }
