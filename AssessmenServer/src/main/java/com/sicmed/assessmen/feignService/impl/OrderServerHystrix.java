2ae942551b9622972f50b4e65b17d69242657565
MaxCoderCh <MaxCoderCh@users.noreply.github.com>
2018-09-13 10:31:44 +0800

    添加 OrderServer 订单查询服务

--- AssessmenServer/src/main/java/com/sicmed/assessmen/feignService/impl/OrderServerHystrix.java
+++ AssessmenServer/src/main/java/com/sicmed/assessmen/feignService/impl/OrderServerHystrix.java
@@ -3,10 +3,17 @@ package com.sicmed.assessmen.feignService.impl;
 import com.sicmed.assessmen.feignService.OrderServer;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
 public class OrderServerHystrix extends BaseServerHystrix implements OrderServer {
     @Override
     public String orderDoneSuccess(String orderId) {
         return resultStr;
+    }
+
+    @Override
+    public Map<String, Object> getOrder(String orderId) {
+        return resultMap;
     }
 }
