bddf753d0c24d283e87b62913640913ced6f25e8
MaxCoderCh <MaxCoderCh@users.noreply.github.com>
2018-09-19 13:40:44 +0800

    集成 spring-retry 框架

--- AssessmenServer/src/main/java/com/sicmed/assessmen/AssessmenApplication.java
+++ AssessmenServer/src/main/java/com/sicmed/assessmen/AssessmenApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
 @EnableHystrix  //@EnableCircuitBreaker  ==  @EnableHystrix
 @EnableFeignClients
 @EnableEurekaServer
 @SpringBootApplication
+@EnableRetry
 public class AssessmenApplication {
 
 	public static void main(String[] args) {
