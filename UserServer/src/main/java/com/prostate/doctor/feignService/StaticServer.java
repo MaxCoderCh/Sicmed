b161e565759b30e5d917085d23afd064e6b536bf
MaxCoderCh <MaxCoderCh@users.noreply.github.com>
2018-09-20 17:18:20 +0800

    删除冗余的 服务 调用 功能

--- UserServer/src/main/java/com/prostate/doctor/feignService/StaticServer.java
+++ UserServer/src/main/java/com/prostate/doctor/feignService/StaticServer.java
@@ -14,33 +14,6 @@ import java.util.Map;
 @FeignClient(value = "stata-server",fallback = StaticServerHystrix.class)
 public interface StaticServer {
 
-    @GetMapping(value = "/anamnesisAllergyDrug/getById")
-    Map getAnamnesisAllergyDrugById(@RequestParam("id") String id);
-
-    @GetMapping(value = "/anamnesisEatingDrug/getById")
-    Map getAnamnesisEatingDrugById(@RequestParam("id") String id);
-
-    @GetMapping(value = "/anamnesisIllness/getById")
-    Map getAnamnesisIllnessById(@RequestParam("id") String id);
-
-    @GetMapping(value = "/surgicalHistory/getById")
-    Map getSurgicalHistoryById(@RequestParam("id") String id);
-
-    @GetMapping(value = "/city/getCityDetail")
-    Map getCityDetail(@RequestParam("cityId") String cityId);
-
-    @GetMapping(value = "/education/getById")
-    Map getEducationById(@RequestParam("id") String id);
-
-    @GetMapping(value = "/bloodGroup/getById")
-    Map getBloodGroupById(@RequestParam("id") String id);
-
-    @GetMapping(value = "/nation/getById")
-    Map getNationById(@RequestParam("id") String id);
-
-    @GetMapping(value = "/profession/getById")
-    Map getProfessionById(@RequestParam("id") String id);
-
     @GetMapping(value = "/hospital/getById")
     Map getHospitalById(@RequestParam("id") String id);
 
--- UserServer/src/main/java/com/prostate/doctor/feignService/impl/StaticServerHystrix.java
+++ UserServer/src/main/java/com/prostate/doctor/feignService/impl/StaticServerHystrix.java
@@ -9,51 +9,6 @@ import java.util.Map;
 public class StaticServerHystrix extends BaseServerHystrix implements StaticServer {
 
     @Override
-    public Map getAnamnesisAllergyDrugById(String id) {
-        return resultMap;
-    }
-
-    @Override
-    public Map getAnamnesisEatingDrugById(String id) {
-        return resultMap;
-    }
-
-    @Override
-    public Map getAnamnesisIllnessById(String id) {
-        return resultMap;
-    }
-
-    @Override
-    public Map getSurgicalHistoryById(String id) {
-        return resultMap;
-    }
-
-    @Override
-    public Map getCityDetail(String cityId) {
-        return resultMap;
-    }
-
-    @Override
-    public Map getEducationById(String id) {
-        return resultMap;
-    }
-
-    @Override
-    public Map getBloodGroupById(String id) {
-        return resultMap;
-    }
-
-    @Override
-    public Map getNationById(String id) {
-        return resultMap;
-    }
-
-    @Override
-    public Map getProfessionById(String id) {
-        return resultMap;
-    }
-
-    @Override
     public Map getHospitalById(String id) {
         return resultMap;
     }
