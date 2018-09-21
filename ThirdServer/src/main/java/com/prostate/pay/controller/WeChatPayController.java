e730e6db987cc56bdf761b67c2cd98b2a716a5f1
MaxCoderCh <MaxCoderCh@users.noreply.github.com>
2018-09-21 18:19:06 +0800

    修改 微信 支付信息 获取 接口

--- ThirdServer/src/main/java/com/prostate/pay/controller/WeChatPayController.java
+++ ThirdServer/src/main/java/com/prostate/pay/controller/WeChatPayController.java
@@ -1,15 +1,12 @@
 package com.prostate.pay.controller;
 
 import com.prostate.common.base.BaseController;
-import com.prostate.feignService.OrderServer;
 import com.prostate.pay.entity.UnifiedOrderEntity;
 import com.prostate.pay.service.WeChatPayService;
 import com.prostate.pay.wxpay.sdk.MyWeChatPayConfig;
 import com.prostate.pay.wxpay.sdk.WXPayUtil;
 import com.prostate.redis.RedisSerive;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.RandomStringUtils;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +23,6 @@ public class WeChatPayController extends BaseController {
     @Autowired
     private WeChatPayService weChatPayService;
 
-
-    @Autowired
-    private OrderServer orderServer;
 
     @Autowired
     private RedisSerive redisSerive;
@@ -71,27 +65,20 @@ public class WeChatPayController extends BaseController {
 
 
     @PostMapping(value = "orderPay")
-    public Map orderPay(String orderId) throws Exception {
-
-        Map<String, Object> reMap = orderServer.getOrder(orderId);
-        Map<String, Object> orderMap = (Map<String, Object>) reMap.get("result");
-
-        log.info("orderMap" + orderMap.toString());
+    public Map orderPay(String orderId, String orderPrice,String openid) {
 
         Map<String, String> data = new HashMap();
         data.put("body", "栗子医学-问诊费用支付");
         data.put("out_trade_no", orderId);
         data.put("fee_type", "CNY");
-        data.put("total_fee", orderMap.get("orderPrice").toString());
+        data.put("total_fee", orderPrice);
         data.put("notify_url", "http://www.qlxlm.com/api-order/order/notify/paymentSuccess");
         data.put("trade_type", "JSAPI");
         data.put("product_id", orderId);
         data.put("device_info", "WEB");
-        data.put("openid", redisSerive.getOpenid());
-        log.info("data" + data.toString());
+        data.put("openid", openid);
 
         Map<String, String> map = weChatPayService.unifiedOrder(data);
-        log.info("map" + map.toString());
 
         if (map.get("return_code").equals("SUCCESS")) {
             Map<String, String> signMap = new HashMap<>();
@@ -100,7 +87,12 @@ public class WeChatPayController extends BaseController {
             signMap.put("timeStamp", String.valueOf(WXPayUtil.getCurrentTimestamp()));
             signMap.put("nonceStr", map.get("nonce_str"));
             signMap.put("signType", "MD5");
-            signMap.put("sign", WXPayUtil.generateSignature(signMap, myWeChatPayConfig.getKey()));
+            try {
+                signMap.put("sign", WXPayUtil.generateSignature(signMap, myWeChatPayConfig.getKey()));
+            } catch (Exception e) {
+                log.error("订单 签名失败");
+                return insertFailedResponse();
+            }
             return insertSuccseeResponse(signMap);
         }
 
